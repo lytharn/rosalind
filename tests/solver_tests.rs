1d0fc9c0d340553183b730426b936407ef827f69
lytharn <lytharn@users.noreply.github.com>
2018-05-21 21:53:44 +0200

    Use example sample in solver tests

--- tests/solver_tests.rs
+++ tests/solver_tests.rs
@@ -4,7 +4,8 @@ extern crate rosalind;
 fn run_dna_x_number_of_dna_characters() {
     let args = vec![String::from("rosalind"), String::from("dna")];
     let config = rosalind::Config::new(&args).unwrap();
-    assert_eq!(rosalind::run(config, "ATGCATGATA"), "4 1 2 3");
+    let input = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
+    assert_eq!(rosalind::run(config, input), "20 12 17 21");
 }
 
 #[test]
