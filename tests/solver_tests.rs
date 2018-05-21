ea1ae39690ba4319d0445e559e4acc1025373736
lytharn <lytharn@users.noreply.github.com>
2018-05-21 22:01:14 +0200

    Extract common code in solver tests

--- tests/solver_tests.rs
+++ tests/solver_tests.rs
@@ -1,16 +1,18 @@
 extern crate rosalind;
 
+fn run(arg: &str, input: &str) -> String {
+    let args = vec![String::from("rosalind"), String::from(arg)];
+    let config = rosalind::Config::new(&args).unwrap();
+    rosalind::run(config, input)
+}
+
 #[test]
 fn run_dna_x_number_of_dna_characters() {
-    let args = vec![String::from("rosalind"), String::from("dna")];
-    let config = rosalind::Config::new(&args).unwrap();
     let input = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
-    assert_eq!(rosalind::run(config, input), "20 12 17 21");
+    assert_eq!(run("dna", input), "20 12 17 21");
 }
 
 #[test]
 fn run_rna_x_dna_into_rna() {
-    let args = vec![String::from("rosalind"), String::from("rna")];
-    let config = rosalind::Config::new(&args).unwrap();
-    assert_eq!(rosalind::run(config, "GATGGAACTTGACTACGTAAATT"), "GAUGGAACUUGACUACGUAAAUU");
+    assert_eq!(run("rna", "GATGGAACTTGACTACGTAAATT"), "GAUGGAACUUGACUACGUAAAUU");
 }
