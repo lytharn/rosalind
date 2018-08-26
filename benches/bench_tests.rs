305ed7ab1583722b9ed9430f3a319140c5ae3fb0
lytharn <lytharn@users.noreply.github.com>
2018-08-26 22:17:02 +0200

    Add benchmark for RNA, RECV and SUBS

--- benches/bench_tests.rs
+++ benches/bench_tests.rs
@@ -10,18 +10,51 @@ fn run(arg: &str, input: &str) -> String {
     rosalind::run(config, input)
 }
 
-#[bench]
-fn bench_dna(b: &mut Bencher) {
-    let dna: String = (0..10000000).map(|n| {
+fn generate_dna(n: usize) -> impl Iterator<Item=char> {
+    (0..n).map(|n| {
         match n%4 {
             0 => 'A',
             1 => 'T',
             2 => 'G',
             _ => 'C'
         }
-    }).collect();
+    })
+}
+
+#[bench]
+fn bench_dna(b: &mut Bencher) {
+    let dna: String = generate_dna(10*1000*1000).collect();
 
     b.iter(|| {
         black_box(run("dna", &dna));
+    });
+}
+
+#[bench]
+fn bench_rna(b: &mut Bencher) {
+    let dna: String = generate_dna(100*1000).collect();
+
+    b.iter(|| {
+        black_box(run("rna", &dna));
+    });
+}
+
+#[bench]
+fn bench_revc(b: &mut Bencher) {
+    let dna: String = generate_dna(100*1000).collect();
+
+    b.iter(|| {
+        black_box(run("revc", &dna));
+    });
+}
+
+#[bench]
+fn bench_subs(b: &mut Bencher) {
+    let dna: String = generate_dna(100*1000)
+        .chain(("\n".to_string()+&"ATGC".repeat(10)).chars())
+        .collect();
+
+    b.iter(|| {
+        black_box(run("subs", &dna));
     });
 }
