d0425ff36208d0b3690b37fdab6ba09093dbedac
lytharn <lytharn@users.noreply.github.com>
2018-09-17 22:46:43 +0200

    Improve parallel DNA performance

--- src/dnap/mod.rs
+++ src/dnap/mod.rs
@@ -4,7 +4,7 @@ mod tests;
 use rayon::prelude::*;
 
 pub fn run(dna: &str) -> String {
-    let (a, c, g, t) = dna.par_bytes().fold(|| (0, 0, 0, 0), |mut acc, byte| {
+    let (a, c, g, t) = dna.as_bytes().par_iter().fold(|| (0, 0, 0, 0), |mut acc, byte| {
         match byte {
             b'A' => acc.0 += 1,
             b'C' => acc.1 += 1,
